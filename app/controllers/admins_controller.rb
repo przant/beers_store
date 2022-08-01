6cc362b412ab492a441b1dbf6d9cda3091295498
Antonio Pérez <apema2079@gmail.com>
2022-08-01 12:52:27 -0500

    Finish create new drink flow from admins view

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,11 +3,28 @@ class AdminsController < ApplicationController
   end
 
   def new_drink
+    @drink = Drink.new
+  end
+
+  def create_drink
+    @drink = Drink.new(drink_params)
+
+    if @drink.save
+      redirect_to dashboard_admins_path, notice: 'Drink was successfully created.'
+    else
+      render :new_drink, status: :unprocessable_entity
+    end
   end
 
   def new_snack
   end
 
   def new_promo
+  end
+
+  private
+
+  def drink_params
+    params.require(:drink).permit(:name, :description, :ingredients, :price)
   end
 end
