c6feecb1c585f3692f58d1f04f71229921ad7634
Antonio Pérez <apema2079@gmail.com>
2022-08-01 02:13:47 -0500

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/drinks', to: 'pages#drinks', as: 'drinks_page'
   get '/snacks', to: 'pages#snacks', as: 'snacks_page'
 
+  get '/dashboard', to: 'admins#dashboard', as: 'dashboard_admins'
+
   root 'home#landing'
 end
