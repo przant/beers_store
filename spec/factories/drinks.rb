8531b694b45bf21f659ff821df5b2b4cecc42977
Antonio Pérez <apema2079@gmail.com>
2022-08-01 18:15:50 -0500

    Update factories

--- spec/factories/drinks.rb
+++ spec/factories/drinks.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :drink do
-    name { "MyString" }
-    description { "MyText" }
-    ingredients { "MyText" }
-    price { 1.5 }
+    sequence(:name) { |n| "SampleDrink##{n}" }
+    sequence(:description) { |n| "Sample description for the Drink ##{n}" }
+    sequence(:ingredients) { |n| "Sample list of ingredients for the Drink##{n}" }
+    price { 20 }
   end
 end
--- spec/factories/promos.rb
+++ spec/factories/promos.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :promo do
-    name { "MyString" }
-    description { "MyText" }
+    sequence(:name) { |n| "SampleSnack##{n}" }
+    sequence(:description) { |n| "Sample description for the Snack ##{n}" }
     active { false }
     price { 1.5 }
+  end
+
+  trait :active_promo do
+    active { true }
   end
 end
