638ad9f06db3584a5d7ef1693c47eef7a01a7b1a
Antonio Pérez <apema2079@gmail.com>
2022-08-01 01:55:51 -0500

    Add comment in seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# The first created user will hav 'Admin' role
 User.create(
   [
     {
