631b4a94afe8d998c73c19c5e8efe96e94c8916c
Antonio Pérez <apema2079@gmail.com>
2022-08-01 13:31:15 -0500

    Complete New Snack pipeline

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -17,6 +17,17 @@ class AdminsController < ApplicationController
   end
 
   def new_snack
+    @snack = Snack.new
+  end
+
+  def create_snack
+    @snack = Snack.new(snack_params)
+
+    if @snack.save
+      redirect_to dashboard_admins_path, notice: 'Snack was successfully created.'
+    else
+      render :new_snack, status: :unprocessable_entity
+    end
   end
 
   def new_promo
@@ -26,5 +37,9 @@ class AdminsController < ApplicationController
 
   def drink_params
     params.require(:drink).permit(:name, :description, :ingredients, :price)
+  end
+
+  def snack_params
+    params.require(:snack).permit(:name, :description, :ingredients, :price)
   end
 end
