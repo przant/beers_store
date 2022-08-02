ec7b8bc5ce766a719e0ad827cb8ad650bd2c9e83
Antonio Pérez <apema2079@gmail.com>
2022-08-02 15:17:31 -0500

    Move snacks methods into snacks controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,35 +1,12 @@
 class AdminsController < ApplicationController
-  def dashboard
-  end
-
-  def snacks
-    @snacks = Snack.all
-  end
+  def dashboard; end
 
   def promos
     @promos = Promo.all
   end
 
-  def snack
-    @snack = Snack.find(params[:id])
-  end
-
   def promo
     @promo = Promo.find(params[:id])
-  end
-
-  def new_snack
-    @snack = Snack.new
-  end
-
-  def create_snack
-    @snack = Snack.new(snack_params)
-
-    if @snack.save
-      redirect_to dashboard_admins_path, notice: 'Snack was successfully created.'
-    else
-      render :new_snack, status: :unprocessable_entity
-    end
   end
 
   def new_promo
@@ -43,20 +20,6 @@ class AdminsController < ApplicationController
       redirect_to dashboard_admins_path, notice: 'Promo was successfully created.'
     else
       render :new_promo, status: :unprocessable_entity
-    end
-  end
-
-  def edit_snack
-    @snack = Snack.find(params[:id])
-  end
-
-  def update_snack
-    @snack = Snack.find(params[:id])
-
-    if @snack.update(snack_params)
-      redirect_to snack_admins_path(@snack), notice: 'Snack was successfully updated.'
-    else
-      render :edit_snack
     end
   end
 
@@ -74,16 +37,6 @@ class AdminsController < ApplicationController
     end
   end
 
-  def delete_snack
-    @snack = Snack.find(params[:id])
-
-    if @snack.destroy
-      redirect_to snacks_admins_path, notice: 'The snack was successfuly removed.'
-    else
-      redirect_to snacks_admins_path, alert: 'Cannot remove the snack.'
-    end
-  end
-
   def delete_promo
     @promo = Promo.find(params[:id])
 
@@ -95,10 +48,6 @@ class AdminsController < ApplicationController
   end
 
   private
-
-  def snack_params
-    params.require(:snack).permit(:name, :description, :ingredients, :price)
-  end
 
   def promo_params
     params.require(:promo).permit(:name, :description, :price, :active)
--- config/routes.rb
+++ config/routes.rb
@@ -16,25 +16,20 @@ Rails.application.routes.draw do
   patch '/admins/drinks/:id/edit', to: 'admins/drinks#update_drink', as: 'update_drink_admins'
   delete '/admins/drinks/:id', to: 'admins/drinks#delete_drink', as: 'delete_drink_admins'
 
-  get '/admin/snacks/', to: 'admins#snacks', as: 'snacks_admins'
-  get '/admin/promos/', to: 'admins#promos', as: 'promos_admins'
-
-  get '/snacks/new', to: 'admins#new_snack', as: 'new_snack_admins'
-  post '/snacks/new', to: 'admins#create_snack', as: 'create_snack_admins'
-
-  get '/promotion/new', to: 'admins#new_promo', as: 'new_promo_admins'
-  post '/promotion/new', to: 'admins#create_promo', as: 'create_promo_admins'
-
+  get '/admin/snacks/', to: 'admins/snacks#snacks', as: 'snacks_admins'
+  get '/snacks/new', to: 'admins/snacks#new_snack', as: 'new_snack_admins'
+  post '/snacks/new', to: 'admins/snacks#create_snack', as: 'create_snack_admins'
   get '/admin/snacks/:id', to: 'admins#snack', as: 'snack_admins'
-  get '/admin/promos/:id', to: 'admins#promo', as: 'promo_admins'
-
   get '/admin/snacks/:id/edit', to: 'admins#edit_snack', as: 'edit_snack_admins'
   patch '/admin/snacks/:id/edit', to: 'admins#update_snack', as: 'update_snack_admins'
+  delete '/admin/snacks/:id', to: 'admins#delete_snack', as: 'delete_snack_admins'
 
+  get '/admin/promos/', to: 'admins#promos', as: 'promos_admins'
+  get '/promotion/new', to: 'admins#new_promo', as: 'new_promo_admins'
+  post '/promotion/new', to: 'admins#create_promo', as: 'create_promo_admins'
+  get '/admin/promos/:id', to: 'admins#promo', as: 'promo_admins'
   get '/admin/promos/:id/edit', to: 'admins#edit_promo', as: 'edit_promo_admins'
   patch '/admin/promos/:id/edit', to: 'admins#update_promo', as: 'update_promo_admins'
-
-  delete '/admin/snacks/:id', to: 'admins#delete_snack', as: 'delete_snack_admins'
   delete '/admin/promos/:id', to: 'admins#delete_promo', as: 'delete_promo_admins'
 
   root 'home#landing'
