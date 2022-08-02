14f50999da8ab688c7ac89d1a5eeff84936e8d47
Antonio Pérez <apema2079@gmail.com>
2022-08-02 14:35:30 -0500

    Create drinks controller and wrap in admins module

---
+++ app/controllers/admins/drinks_controller.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module Admins
+  class DrinksController < ApplicationController
+    def drinks
+      @drinks = Drink.all
+    end
+
+    def drink
+      @drink = Drink.find(params[:id])
+    end
+
+    def new_drink
+      @drink = Drink.new
+    end
+
+    def create_drink
+      @drink = Drink.new(drink_params)
+
+      if @drink.save
+        redirect_to dashboard_admins_path, notice: 'Drink was successfully created.'
+      else
+        render :new_drink, status: :unprocessable_entity
+      end
+    end
+
+    def edit_drink
+      @drink = Drink.find(params[:id])
+    end
+
+    def update_drink
+      @drink = Drink.find(params[:id])
+
+      if @drink.update(drink_params)
+        redirect_to drink_admins_path(@drink), notice: 'Drink was successfully updated.'
+      else
+        render :edit_drink
+      end
+    end
+
+    def delete_drink
+      @drink = Drink.find(params[:id])
+
+      if @drink.destroy
+        redirect_to drinks_admins_path, notice: 'The drink was successfuly removed.'
+      else
+        redirect_to drinks_admins_path, alert: 'Cannot remove the drink.'
+      end
+    end
+
+    private
+
+    def drink_params
+      params.require(:drink).permit(:name, :description, :ingredients, :price)
+    end
+  end
+end
